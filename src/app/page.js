5e940b71abbbdb4e569a1237c92432a7cc76485e
nazir17 <naziruddin@tothebyte.com>
2025-08-13 17:41:48 +0530

    fixed responsivenes

--- src/app/page.js
+++ src/app/page.js
@@ -1,134 +1,133 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (<>
+  return (
+    <>
     
-          <header>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Grid Layout</title>
-          </header>
+      <header>
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+        <title>Grid Layout</title>
+      </header>
 
-          
+      
+      <nav className={styles.nav}>
+        <div className={styles.container}>
+          <div>Layout 1</div>
+          <div>About</div>
+          <div>Services</div>
+          <div>Contact</div>
+        </div>
+      </nav>
+
+      
+      <header className={styles.pageheader}>
+        <div><h1>
+          Page Heading <span>Secondary Text</span>
+        </h1></div>
+      </header>
+
+      
+      <section className={styles.projectcontainer}>
+        <div className={styles.grid}>
     
-
-          <nav className={styles.nav}>
-              <div className={styles.container}>
-                      <div>Layout 1</div>
-                      <div>About</div>
-                      <div>Services</div>
-                      <div>Contact</div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
               </div>
-          </nav>
-
-    
-          <header className={styles.pageheader}>
-              <h1>Page Heading <span>Secondary Text</span></h1>
-          </header>
-
-    
-          <section className={styles.projectcontainer}>
-              <div className={styles.grid}>
-                  
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
-                  <div className={styles.card}>
-                      <Image 
-                      src="/700x400.svg" 
-                      width={400}
-                      height={200}
-                      alt="Project Image"/>
-                      <h3>Project Name</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        <br></br>Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                  </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
               </div>
-          </section>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.placeholder}>700 x 400</div>
+                <h3>Project Name</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  <br />
+                  Nam viverra euismod odio, gravida pellentesque urna varius
+                  vitae.
+                </p>
+              </div>
+            
+        </div>
+      </section>
 
-    
-          <footer className={styles.footer}>
-              <p>Copyright © Your Website 2014</p>
-          </footer>
-          
-    
+      
+      <footer className={styles.footer}>
+        <p>Copyright © Your Website 2014</p>
+      </footer>
     </>
   );
 }
